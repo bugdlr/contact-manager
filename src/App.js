a50f207e8ec66c2aa12315f926f350db650fa2b0
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-24 11:57:49 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <Header branding="Contact Manager"/>
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+          <Contact name="Susan Smith" email="ssmith@gmail.com" phone="333-333-3333" />
+        </div>
       </div>
     );
   }
