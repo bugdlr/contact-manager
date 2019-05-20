dc46c1269397b7bf63929d00827233bbdccba1e2
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-05-20 12:42:51 -0700

    Updates

---
+++ precache-manifest.b1e2f01a46b2da7b448eedf4510056e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f0f0b95136c69c9a55b004bd2f5398",
+    "url": "/bugdlr/contact-manager/index.html"
+  },
+  {
+    "revision": "2de9810d0b4057d62028",
+    "url": "/bugdlr/contact-manager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "897140789bde2c50f85f",
+    "url": "/bugdlr/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2de9810d0b4057d62028",
+    "url": "/bugdlr/contact-manager/static/js/2.d3283c91.chunk.js"
+  },
+  {
+    "revision": "897140789bde2c50f85f",
+    "url": "/bugdlr/contact-manager/static/js/main.1e14946c.chunk.js"
+  },
+  {
+    "revision": "cf125224da5ea303f39b",
+    "url": "/bugdlr/contact-manager/static/js/runtime~main.c5a9220b.js"
+  }
+]);
